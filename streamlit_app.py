26867a9300f4903bf9d22a4bba7cfe43038f0957
pejuzb <peto.jurko@gmail.com>
2024-09-23 21:27:08 +0200

    Final clean

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,15 @@ import yaml
 import bcrypt as bc
 
 # Load the secrets.yaml file
+
 def load_secrets():
     with open("secrets.yaml", "r") as file:
         return yaml.safe_load(file)
 
 secrets = load_secrets()
 
-
 #Password hashing
+
 def hash_password(password):
     salt = bc.gensalt()  # Generate salt
     hashed_password = bc.hashpw(password.encode('utf-8'), salt)  # Hash password
@@ -22,33 +23,7 @@ def check_password(username, entered_password):
     return bc.checkpw(entered_password.encode('utf-8'), stored_password_hash.encode('utf-8'))
 
 
-st.write(hash_password('oco'))
-
-
-
-# test_pass = st.text_input('Enter test password')
-# st.button('Check password')
-
-# if st.button:
-#     if check_password(stored_password_hash, test_pass):
-#         st.write('Password is correct')
-#     else:
-#         st.write('Password is incorrect')
-
-#Password hashing test 
-
-
-
-
-# Login - authentication
-# def authenticate(username, password):
-#     if username in secrets['users']:
-#         stored_password = secrets['users'][username]['password']
-#         if stored_password == password:
-#             return True
-#         else:
-#             return 
-#     return False
+# Role setup
 
 def role_lookup(username):
     if username in secrets['users']:
@@ -57,6 +32,10 @@ def role_lookup(username):
 
 if "role" not in st.session_state:
     st.session_state.role = None
+
+
+
+#Login and logout
 
 def login():
     st.header("Log in")
@@ -82,11 +61,6 @@ def login():
 def logout():
     st.session_state.role = None
     st.rerun()
-
-
-
-
-
 
 role = st.session_state.role
 
@@ -141,68 +115,3 @@ else:
     pg = st.navigation([st.Page(login)])
 
 pg.run()
-
-
-
-# #A simple in-memory user database
-# users_db = {
-#     "admin": sha256("admin".encode()).hexdigest(),
-#     "user1": sha256("mypassword".encode()).hexdigest()
-# }
-
-# def authenticate(username, password):
-#     """Function to check the username and password."""
-#     password_hash = sha256(password.encode()).hexdigest()
-#     if username in users_db and users_db[username] == password_hash:
-#         return True
-#     return False
-
-# def main():
-#     """Main function to render the Streamlit app."""
-#     st.title("Please login and continue")
-
-#     # Initialize session state for login status
-#     if 'logged_in' not in st.session_state:
-#         st.session_state.logged_in = False
-#         st.session_state.username = ""
-#         st.session_state.current_page = "Home"  # Track the current page
-
-#     # Sidebar: If not logged in, show login form
-#     if not st.session_state.logged_in:
-#         st.sidebar.subheader("Login")
-#         username = st.sidebar.text_input("Username")
-#         password = st.sidebar.text_input("Password", type="password")
-#         login_button = st.sidebar.button("Login")
-
-#         if login_button:
-#             if authenticate(username, password):
-#                 st.session_state.logged_in = True
-#                 st.session_state.username = username
-#                 st.sidebar.success(f"Welcome, {username}!")
-#             else:
-#                 st.sidebar.error("Invalid credentials. Please try again.")
-
-#     # Sidebar: If logged in, show clickable navigation buttons
-#     if st.session_state.logged_in:
-#         # Show Logout Button
-#         if st.sidebar.button("Logout"):
-#             st.session_state.logged_in = False
-#             st.session_state.username = ""
-#             st.session_state.current_page = "Home"
-#             st.sidebar.info("You have logged out.")
-
-
-#         tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-
-#         with tab1:
-#             st.header("A cat")
-#             st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-#         with tab2:
-#             st.header("A dog")
-#             st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-#         with tab3:
-#             st.header("An owl")
-#             st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-
-# if __name__ == "__main__":
-#     main()
