93090e314e06ac18fb0abab57bdc1e27b82be397
pejuzb <peto.jurko@gmail.com>
2024-10-03 20:13:27 +0200

    * respond upload

--- respond/respond_1.py
+++ respond/respond_1.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
-st.header("Respond 1")
+st.header("Upload a files")
 st.write(f"You are logged in as {st.session_state.role}.")
+
+uploaded_files = st.file_uploader(
+    "Choose a CSV file", accept_multiple_files=True
+)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
