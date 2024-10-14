0f0a573ed81534ee716d19762615511bafd7db34
Peter Jurko <36472683+pejuzb@users.noreply.github.com>
2024-10-14 12:47:47 +0200

    Update respond_2.py

--- respond/respond_2.py
+++ respond/respond_2.py
@@ -1,26 +1,45 @@
-import streamlit as st
+from datetime import datetime, timedelta
+
 import pandas as pd
-import mitosheet
-from streamlit.components.v1 import iframe
+import streamlit as st
+from mitosheet.streamlit.v1 import spreadsheet
+from mitosheet.streamlit.v1.spreadsheet import _get_mito_backend
 
-# Display Streamlit app title
-st.title("Mito Pivot Table in Streamlit")
+st.set_page_config(layout="wide")
 
-# Load a sample DataFrame (You can replace this with your actual DataFrame)
-data = {'Category': ['A', 'B', 'A', 'B', 'A', 'B'],
-        'Values': [100, 200, 150, 220, 120, 180],
-        'Sales': [400, 300, 350, 320, 330, 310]}
+@st.cache_data
+def get_tesla_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv')
+    df = df.drop(0)
+    df['volume'] = df['volume'].astype(float)
+    return df
 
-df = pd.DataFrame(data)
+tesla_data = get_tesla_data()
 
-# Create a MitoSheet instance
-mitosheet = MitoSheet()
+new_dfs, code = spreadsheet(tesla_data)
+code = code if code else "# Edit the spreadsheet above to generate code"
+st.code(code)
 
-# Load your dataframe into the MitoSheet
-mitosheet.df = df
+def clear_mito_backend_cache():
+    _get_mito_backend.clear()
 
-# Display the Mito pivot table
-mitosheet.launch()
+# Function to cache the last execution time - so we can clear periodically
+@st.cache_resource
+def get_cached_time():
+    # Initialize with a dictionary to store the last execution time
+    return {"last_executed_time": None}
 
-# Export the Mito iframe to display in Streamlit
-iframe("mitosheet", width=700, height=500)
+def try_clear_cache():
+
+    # How often to clear the cache
+    CLEAR_DELTA = timedelta(hours=12)
+
+    current_time = datetime.now()
+    cached_time = get_cached_time()
+
+    # Check if the current time is different from the cached last execution time
+    if cached_time["last_executed_time"] is None or cached_time["last_executed_time"] + CLEAR_DELTA < current_time:
+        clear_mito_backend_cache()
+        cached_time["last_executed_time"] = current_time
+
+try_clear_cache()
