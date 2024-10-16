12d7efd681a7dfbcef5f875d55c59ee6c0a8dd53
pejuzb <peto.jurko@gmail.com>
2024-10-16 23:14:01 +0200

    * timestamp

--- respond/respond_1.py
+++ respond/respond_1.py
@@ -124,7 +124,7 @@ def load_data():
         b.L2,
         b.L3,
         'Jan' as OWNER,
-        current_date() as LOAD_DATETIME
+        current_timestamp as LOAD_DATETIME
 
         from test as a
         left join (Select * from BUDGET.CORE.HIERARCHY where owner = 'Jan') as b
