b9bb37d8ec0828e756024321792fe17b78a36768
pejuzb <peto.jurko@gmail.com>
2024-10-19 17:37:42 +0200

    + test res_2

--- respond/respond_2.py
+++ respond/respond_2.py
@@ -1,6 +1,66 @@
 import streamlit as st
+from azure.storage.blob import BlobServiceClient
+import os
+from azure.identity import ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+from dotenv import load_dotenv
+import snowflake.connector
+import pandas as pd
+from snowflake.connector.pandas_tools import write_pandas
+load_dotenv()
+from datetime import datetime
+import pytz  # Importing pytz for timezone handling
+import time
+from io import StringIO
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "8%")
-col3.metric("Humidity", "86%", "-4%")
+# Azure Key Vault
+client_id = os.getenv('AZURE_CLIENT_ID')
+tenant_id = os.getenv('AZURE_TENANT_ID')
+client_secret = os.getenv('AZURE_CLIENT_SECRET')
+vault_url = os.getenv('AZURE_VAULT_URL')
+
+# Create a credential object
+credentials = ClientSecretCredential(
+    client_id=client_id,
+    tenant_id=tenant_id,
+    client_secret=client_secret
+)
+
+# Function to get secrets from Azure Key Vault
+def secrets_get(secret_name):
+    secret_client = SecretClient(vault_url=vault_url, credential=credentials)
+    secret = secret_client.get_secret(secret_name)
+    return secret.value
+
+
+# Snowflake connection
+conn = snowflake.connector.connect(
+    user=secrets_get('snf-user'),
+    password=secrets_get('snf-password'),
+    account=secrets_get('snf-account'),
+    #warehouse='COMPUTE_WH',
+    database='BUDGET',
+    schema='RAW',
+    role='ACCOUNTADMIN'
+)
+
+
+cur = conn.cursor()
+
+# Query to fetch data from Snowflake
+query = "Select * from BUDGET.MART.BUDGET where owner = 'Peter'"
+
+# Load data into Pandas DataFrame
+df = pd.read_sql(query, conn)
+
+# Display the DataFrame using Streamlit
+st.title('Snowflake Data Viewer')
+st.write("Here is the data from Snowflake:")
+st.dataframe(df)
+
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+
+
