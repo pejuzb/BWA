c68b5324354412517e83c1de1ab2b25bde282cbf
pejuzb <peto.jurko@gmail.com>
2024-10-19 09:41:26 +0200

    * better day

--- respond/respond_1.py
+++ respond/respond_1.py
@@ -140,8 +140,8 @@ def load_data():
         b.L1,
         b.L2,
         b.L3,
-        'Jan' as OWNER,
-        TO_CHAR(current_timestamp, 'YYYY-MM-DD HH24:MI:SS')  as LOAD_DATETIME
+        'Jan' as OWNER
+        --TO_CHAR(current_timestamp, 'YYYY-MM-DD HH24:MI:SS')  as LOAD_DATETIME
 
         from test as a
         left join (Select * from BUDGET.CORE.HIERARCHY where owner = 'Jan') as b
@@ -170,6 +170,7 @@ edited_df = st.data_editor(df, num_rows="dynamic")
 
 # Button to insert updated data
 if st.button("Insert Data into Snowflake"):
+    edited_df['LOAD_DATETIME'] = datetime.now(pytz.timezone('Europe/Prague')).strftime('%Y-%m-%d %H:%M:%S')
     insert_data(edited_df)
 
 
