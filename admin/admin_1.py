f25d85e18cebb05763c7a645a1780c6f94284ba0
pejuzb <peto.jurko@gmail.com>
2024-10-21 21:16:17 +0200

    + admin upload

--- admin/admin_1.py
+++ admin/admin_1.py
@@ -1,4 +1,222 @@
 import streamlit as st
+from azure.storage.blob import BlobServiceClient
+import os
+from azure.identity import ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+from dotenv import load_dotenv
+import snowflake.connector
+import pandas as pd
+from snowflake.connector.pandas_tools import write_pandas
+load_dotenv()
+from datetime import datetime
+import pytz  # Importing pytz for timezone handling
+import time
+from io import StringIO
 
-st.header("Admin 1")
-st.write(f"You are logged in as {st.session_state.role}.")
+# Azure Key Vault
+client_id = os.getenv('AZURE_CLIENT_ID')
+tenant_id = os.getenv('AZURE_TENANT_ID')
+client_secret = os.getenv('AZURE_CLIENT_SECRET')
+vault_url = os.getenv('AZURE_VAULT_URL')
+
+# Create a credential object
+credentials = ClientSecretCredential(
+    client_id=client_id,
+    tenant_id=tenant_id,
+    client_secret=client_secret
+)
+
+# Function to get secrets from Azure Key Vault
+def secrets_get(secret_name):
+    secret_client = SecretClient(vault_url=vault_url, credential=credentials)
+    secret = secret_client.get_secret(secret_name)
+    return secret.value
+
+# Function to upload file to Azure Blob Storage
+def upload_to_blob(file, filename):
+    try:
+        # Create a blob service client using the connection string
+        blob_service_client = BlobServiceClient(account_url=secrets_get("sc-storage"), credential=credentials)
+
+        # Create a blob client
+        blob_client = blob_service_client.get_container_client(container='snfdb')
+
+        # Read the file content
+        file_data = file.read()
+
+        # Upload the file content to the blob
+        blob_client.upload_blob(data=file_data, name=filename, overwrite=True)
+        return f"File {filename} uploaded successfully!"
+    except Exception as e:
+        return f"Error uploading file: {e}"
+
+# Streamlit File Uploader for multiple files
+st.title("Upload Files to Azure Blob Storage")
+
+uploaded_files = st.file_uploader("Choose files", type=['csv', 'txt', 'pdf', 'jpg', 'png'], accept_multiple_files=True)
+
+if uploaded_files is not None:
+    for uploaded_file in uploaded_files:
+        # Get the file details
+        file_details = {
+            "filename": uploaded_file.name,
+            "filetype": uploaded_file.type,
+            "filesize": uploaded_file.size
+        }
+
+        # Display file details
+        st.write(file_details)
+
+        # Upload the file to Azure Blob Storage
+        result_message = upload_to_blob(uploaded_file, uploaded_file.name)
+        st.write(result_message)
+
+# Snowflake connection
+conn = snowflake.connector.connect(
+    user=secrets_get('snf-user'),
+    password=secrets_get('snf-password'),
+    account=secrets_get('snf-account'),
+    #warehouse='COMPUTE_WH',
+    database='BUDGET',
+    schema='RAW',
+    role='ACCOUNTADMIN'
+)
+
+
+cur = conn.cursor()
+
+if st.button("Load files"):
+    try:
+        # Execute the stored procedures
+        cur.execute("CALL COPY_FILES_TO_RAW_REVOLUT();")
+        st.write("First stored procedure [REVOLUT] executed successfully!")
+
+        cur.execute("CALL COPY_FILES_TO_RAW_CSOB();")
+        st.write("Second stored procedure [CSOB] executed successfully!")
+
+    except Exception as e:
+        st.write(f"Error: {e}")  # Display error message if any
+
+
+# Query to fetch data from Snowflake
+query = "Select * from BUDGET.CORE.HIERARCHY where owner = 'Peter'"
+
+# Load data into Pandas DataFrame
+df = pd.read_sql(query, conn)
+
+# Display the DataFrame using Streamlit
+st.title('Snowflake Data Viewer')
+st.write("Here is the data from Snowflake:")
+st.dataframe(df)
+
+
+
+
+# Query to fetch data from Snowflake
+query_mh = """Select * from BUDGET.MART.BUDGET where owner = 'Peter' and L1 is null
+order by transaction_date desc;"""
+
+# Load data into Pandas DataFrame
+df_mh = pd.read_sql(query_mh, conn)
+
+# Display the DataFrame using Streamlit
+st.title('Record with Missing Hierarchy')
+st.write("Transaction data with missing hierarchy:")
+st.dataframe(df_mh)
+
+
+
+
+# Function to query data from Snowflake
+@st.cache_data  # Caches the data to avoid querying every time
+def load_data():
+        query = """with test as (
+        Select distinct prod_hierarchy,source_system from BUDGET.CORE.TRANSACTION as a
+        where a.owner = 'Peter'
+        )
+
+        Select
+        --a.source_system,
+        MD5(a.prod_hierarchy) as HIERARCHY_HK,
+        a.prod_hierarchy as PROD_HIERARCHY_ID,
+        b.L1,
+        b.L2,
+        b.L3,
+        'Peter' as OWNER
+
+        from test as a
+        left join (Select * from BUDGET.CORE.HIERARCHY where owner = 'Peter') as b
+        on a.prod_hierarchy = b.prod_hierarchy_id
+        where HIERARCHY_HK is null
+        order by 1,2"""
+        
+        df = pd.read_sql(query, conn)
+        return df
+
+# Function to insert DataFrame back into Snowflake
+def insert_data(df):
+    conn.cursor().execute("USE SCHEMA CORE")
+    success, nchunks, nrows, _ = write_pandas(conn, df, 'HIERARCHY')
+    if success:
+        st.success(f"Successfully inserted {nrows} rows into Snowflake!")
+        st.cache_data.clear()
+    else:
+        st.error("Failed to insert data.")
+
+# Load data from Snowflake
+df = load_data()
+
+# Display editable DataFrame
+st.write("### Editable Table")
+edited_df = st.data_editor(df, num_rows="dynamic")
+
+
+def export_csv():
+    try:
+        # Create a blob service client using the connection string or account URL
+        blob_service_client = BlobServiceClient(account_url=secrets_get("sc-storage"), credential=credentials)
+
+        # Create a container client for the specified container (without the 'blob' argument)
+        container_client = blob_service_client.get_container_client('snfdb')
+
+        # Create a blob client for the specific blob (file) you want to upload
+        blob_client = container_client.get_blob_client('azure_export_peter.csv')
+
+
+        # Query to fetch data from Snowflake
+        query_hier = "Select * from BUDGET.CORE.HIERARCHY where owner = 'Peter'"
+
+        # Load data into Pandas DataFrame
+        df_H = pd.read_sql(query_hier, conn)
+
+        if df_H.empty:
+            st.write("No data to export. The DataFrame is empty.")
+            return  # This terminates the process if no data is available
+
+        # Convert DataFrame to CSV in memory
+        csv_buffer = StringIO()
+        df_H.to_csv(csv_buffer, index=False)
+
+        # Upload the CSV to Azure Blob Storage
+        blob_client.upload_blob(csv_buffer.getvalue(), overwrite=True)
+
+        st.write("CSV exported successfully!")
+        
+    except Exception as e:
+        st.write(f"Error exporting data: {e}")
+
+
+# Button to insert updated data
+if st.button("Insert Data into Snowflake"):
+    edited_df['LOAD_DATETIME'] = datetime.now(pytz.timezone('Europe/Prague')).strftime('%Y-%m-%d %H:%M:%S')
+    edited_df = edited_df[edited_df['L1'].notnull()]
+    insert_data(edited_df)
+    export_csv()
+
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+
+
+
--- admin/admin_2.py
+++ admin/admin_2.py
@@ -1,4 +1,151 @@
 import streamlit as st
+from azure.storage.blob import BlobServiceClient
+import os
+from azure.identity import ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+from dotenv import load_dotenv
+import snowflake.connector
+import pandas as pd
+from snowflake.connector.pandas_tools import write_pandas
+load_dotenv()
+from st_aggrid import AgGrid, GridOptionsBuilder  #add import for GridOptionsBuilder
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.ticker as ticker
+import altair as alt
 
-st.header("Admin 2")
-st.write(f"You are logged in as {st.session_state.role}.")
+
+# Azure Key Vault
+client_id = os.getenv('AZURE_CLIENT_ID')
+tenant_id = os.getenv('AZURE_TENANT_ID')
+client_secret = os.getenv('AZURE_CLIENT_SECRET')
+vault_url = os.getenv('AZURE_VAULT_URL')
+
+# Create a credential object
+credentials = ClientSecretCredential(
+    client_id=client_id,
+    tenant_id=tenant_id,
+    client_secret=client_secret
+)
+
+# Function to get secrets from Azure Key Vault
+def secrets_get(secret_name):
+    secret_client = SecretClient(vault_url=vault_url, credential=credentials)
+    secret = secret_client.get_secret(secret_name)
+    return secret.value
+
+
+# Snowflake connection
+conn = snowflake.connector.connect(
+    user=secrets_get('snf-user'),
+    password=secrets_get('snf-password'),
+    account=secrets_get('snf-account'),
+    #warehouse='COMPUTE_WH',
+    database='BUDGET',
+    schema='RAW',
+    role='ACCOUNTADMIN'
+)
+
+
+cur = conn.cursor()
+
+# Query to fetch data from Snowflake
+query = "Select * from BUDGET.MART.BUDGET where owner = 'Peter'"
+
+# Load data into Pandas DataFrame
+data = pd.read_sql(query, conn)
+
+# Display the DataFrame using Streamlit
+st.title('Peters Budget Data Viewer')
+#AgGrid(data, height=400)
+
+
+# # Filter out rows where L1 is 'TD Synnex'
+# df_filtered = data[(data['L1'] != 'TD Synnex') & (data['YEAR'] == 2024)]
+# #Group by L1 and month, and sum the amounts
+# monthly_expenses = df_filtered.groupby(['L1','REPORTING_DATE'])['AMOUNT'].sum()
+# # Take absolute values of sums
+# monthly_expenses = monthly_expenses.abs()
+# #st.dataframe(monthly_expenses)
+
+
+with st.container(border=True):
+    #st.write("This is inside the container")  
+    data_chart = pd.read_sql("""Select 
+        REPORTING_DATE,
+        L1,
+        ABS(SUM(AMOUNT)) as AMOUNT FROM BUDGET.MART.BUDGET
+        WHERE L1 <> 'TD Synnex' and YEAR = YEAR(current_date) and OWNER = 'Peter' 
+        GROUP BY ALL;""", conn)
+    
+    
+    # Create an Altair bar chart
+    chart = alt.Chart(data_chart).mark_bar(size=25).encode(
+        x='REPORTING_DATE:T',
+        y='AMOUNT:Q',
+        color='L1:N'
+    ).properties(
+        width=600,  # Set the width of the chart
+        height=400  # Set the height of the chart
+    ).configure_axis(
+        labelFontSize=14,  # Adjust axis label size
+        titleFontSize=16,  # Adjust axis title size
+    ).configure_legend(
+        titleFontSize=16,  # Adjust legend title size
+        labelFontSize=14   # Adjust legend label size
+    )
+    
+    # Display the chart in Streamlit
+    st.altair_chart(chart, use_container_width=True)
+
+
+
+with st.container(border=True):
+    #st.write("This is inside the container")  
+
+    data_chart_2 = pd.read_sql("""Select 
+        REPORTING_DATE,
+        SUM(AMOUNT) as AMOUNT FROM BUDGET.MART.BUDGET
+        WHERE YEAR = YEAR(current_date) and OWNER = 'Peter' 
+        GROUP BY ALL;""", conn)
+    
+    
+    # Add a color column based on the AMOUNT value
+    data_chart_2['color'] = data_chart_2['AMOUNT'].apply(lambda x: 'green' if x > 0 else 'red')
+    
+    # Create an Altair bar chart
+    chart_2 = alt.Chart(data_chart_2).mark_bar(size = 25).encode(
+        x='REPORTING_DATE:T',
+        y='AMOUNT:Q',
+        color=alt.condition(
+            alt.datum.AMOUNT > 0,  # Condition for positive values
+            alt.value('green'),     # Color if condition is true
+            alt.value('red')        # Color if condition is false
+        )
+    ).properties(
+        width=600,
+        height=400
+    
+    ).configure_axis(
+        labelFontSize=14,  # Adjust axis label size
+        titleFontSize=16,  # Adjust axis title size
+    ).configure_legend(
+        titleFontSize=16,  # Adjust legend title size
+        labelFontSize=14   # Adjust legend label size
+    )
+
+    # Display the chart in Streamlit
+    st.altair_chart(chart_2, use_container_width=True)
+
+with st.container(border=True):
+    st.line_chart(data_chart, x="REPORTING_DATE", y="AMOUNT", color="L1")
+
+#st.write("Here is the raw data from Snowflake:")
+st.dataframe(data)
+
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+
+
