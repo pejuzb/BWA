243135c3a786f5b89299db0b01d66e4e452ad2aa
Peter Jurko <36472683+pejuzb@users.noreply.github.com>
2024-10-21 15:51:59 +0200

    * borders

--- respond/respond_2.py
+++ respond/respond_2.py
@@ -57,86 +57,88 @@ data = pd.read_sql(query, conn)
 
 # Display the DataFrame using Streamlit
 st.title('Peters Budget Data Viewer')
-
 #AgGrid(data, height=400)
 
 
-# Filter out rows where L1 is 'TD Synnex'
-df_filtered = data[(data['L1'] != 'TD Synnex') & (data['YEAR'] == 2024)]
-#Group by L1 and month, and sum the amounts
-monthly_expenses = df_filtered.groupby(['L1','REPORTING_DATE'])['AMOUNT'].sum()
-# Take absolute values of sums
-monthly_expenses = monthly_expenses.abs()
-#st.dataframe(monthly_expenses)
+# # Filter out rows where L1 is 'TD Synnex'
+# df_filtered = data[(data['L1'] != 'TD Synnex') & (data['YEAR'] == 2024)]
+# #Group by L1 and month, and sum the amounts
+# monthly_expenses = df_filtered.groupby(['L1','REPORTING_DATE'])['AMOUNT'].sum()
+# # Take absolute values of sums
+# monthly_expenses = monthly_expenses.abs()
+# #st.dataframe(monthly_expenses)
 
 
-data_chart = pd.read_sql("""Select 
-    REPORTING_DATE,
-    L1,
-    ABS(SUM(AMOUNT)) as AMOUNT FROM BUDGET.MART.BUDGET
-    WHERE L1 <> 'TD Synnex' and YEAR = YEAR(current_date) and OWNER = 'Peter' 
-    GROUP BY ALL;""", conn)
-
-
-# Create an Altair bar chart
-chart = alt.Chart(data_chart).mark_bar(size=25).encode(
-    x='REPORTING_DATE:T',
-    y='AMOUNT:Q',
-    color='L1:N'
-).properties(
-    width=600,  # Set the width of the chart
-    height=400  # Set the height of the chart
-).configure_axis(
-    labelFontSize=14,  # Adjust axis label size
-    titleFontSize=16,  # Adjust axis title size
-).configure_legend(
-    titleFontSize=16,  # Adjust legend title size
-    labelFontSize=14   # Adjust legend label size
-)
-
-# Display the chart in Streamlit
-st.altair_chart(chart, use_container_width=True)
-
-
-
-
-
-data_chart_2 = pd.read_sql("""Select 
-    REPORTING_DATE,
-    SUM(AMOUNT) as AMOUNT FROM BUDGET.MART.BUDGET
-    WHERE YEAR = YEAR(current_date) and OWNER = 'Peter' 
-    GROUP BY ALL;""", conn)
-
-
-# Add a color column based on the AMOUNT value
-data_chart_2['color'] = data_chart_2['AMOUNT'].apply(lambda x: 'green' if x > 0 else 'red')
-
-# Create an Altair bar chart
-chart_2 = alt.Chart(data_chart_2).mark_bar(size = 25).encode(
-    x='REPORTING_DATE:T',
-    y='AMOUNT:Q',
-    color=alt.condition(
-        alt.datum.AMOUNT > 0,  # Condition for positive values
-        alt.value('green'),     # Color if condition is true
-        alt.value('red')        # Color if condition is false
+with st.container(border=True):
+    st.write("This is inside the container")  
+    data_chart = pd.read_sql("""Select 
+        REPORTING_DATE,
+        L1,
+        ABS(SUM(AMOUNT)) as AMOUNT FROM BUDGET.MART.BUDGET
+        WHERE L1 <> 'TD Synnex' and YEAR = YEAR(current_date) and OWNER = 'Peter' 
+        GROUP BY ALL;""", conn)
+    
+    
+    # Create an Altair bar chart
+    chart = alt.Chart(data_chart).mark_bar(size=25).encode(
+        x='REPORTING_DATE:T',
+        y='AMOUNT:Q',
+        color='L1:N'
+    ).properties(
+        width=600,  # Set the width of the chart
+        height=400  # Set the height of the chart
+    ).configure_axis(
+        labelFontSize=14,  # Adjust axis label size
+        titleFontSize=16,  # Adjust axis title size
+    ).configure_legend(
+        titleFontSize=16,  # Adjust legend title size
+        labelFontSize=14   # Adjust legend label size
     )
-).properties(
-    width=600,
-    height=400
-
-).configure_axis(
-    labelFontSize=14,  # Adjust axis label size
-    titleFontSize=16,  # Adjust axis title size
-).configure_legend(
-    titleFontSize=16,  # Adjust legend title size
-    labelFontSize=14   # Adjust legend label size
-)
-
-# Display the chart in Streamlit
-st.altair_chart(chart_2, use_container_width=True)
+    
+    # Display the chart in Streamlit
+    st.altair_chart(chart, use_container_width=True)
 
 
-st.line_chart(data_chart, x="REPORTING_DATE", y="AMOUNT", color="L1")
+
+with st.container(border=True):
+    st.write("This is inside the container")  
+
+    data_chart_2 = pd.read_sql("""Select 
+        REPORTING_DATE,
+        SUM(AMOUNT) as AMOUNT FROM BUDGET.MART.BUDGET
+        WHERE YEAR = YEAR(current_date) and OWNER = 'Peter' 
+        GROUP BY ALL;""", conn)
+    
+    
+    # Add a color column based on the AMOUNT value
+    data_chart_2['color'] = data_chart_2['AMOUNT'].apply(lambda x: 'green' if x > 0 else 'red')
+    
+    # Create an Altair bar chart
+    chart_2 = alt.Chart(data_chart_2).mark_bar(size = 25).encode(
+        x='REPORTING_DATE:T',
+        y='AMOUNT:Q',
+        color=alt.condition(
+            alt.datum.AMOUNT > 0,  # Condition for positive values
+            alt.value('green'),     # Color if condition is true
+            alt.value('red')        # Color if condition is false
+        )
+    ).properties(
+        width=600,
+        height=400
+    
+    ).configure_axis(
+        labelFontSize=14,  # Adjust axis label size
+        titleFontSize=16,  # Adjust axis title size
+    ).configure_legend(
+        titleFontSize=16,  # Adjust legend title size
+        labelFontSize=14   # Adjust legend label size
+    )
+
+    # Display the chart in Streamlit
+    st.altair_chart(chart_2, use_container_width=True)
+
+with st.container(border=True):
+    st.line_chart(data_chart, x="REPORTING_DATE", y="AMOUNT", color="L1")
 
 st.write("Here is the raw data from Snowflake:")
 st.dataframe(data)
